62f50f24f23c2cdab65e464f2bc04f1333e8f92a
kbarwinski <kajetan.barwinski@outlook.com>
2022-09-04 16:56:15 +0200

    [FEATURE] Added Field class w/ FieldType enum and corresponding unit tests.

---
+++ Battleships_TDD/Field.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleships_TDD
+{
+    public class Field
+    {
+        public Field(int row, int column)
+        {
+            if (row < 0 || column < 0)
+                throw new ArgumentOutOfRangeException("At least one of the provided parameters is out of range.");
+
+            Row = row;
+            Column = column;
+            FieldType = FieldType.Empty;
+        }
+
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public FieldType FieldType { get; set; }
+    }
+}
---
+++ Battleships_TDD_Tests/Field_Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Battleships_TDD;
+namespace Battleships_TDD_Tests
+{
+    public class Field_Tests
+    {
+        [Theory]
+        [InlineData(-1,0)]
+        [InlineData(0,-1)]
+        [InlineData(-1,-1)]
+        public void Field_InvalidRowAndOrColumn_ThrowsArgumentOutOfRangeException(int row, int column)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Field(row, column));
+        }
+
+        [Theory]
+        [InlineData(1,2)]
+        public void Field_ValidArguments_ReturnsEmptyField(int row, int column)
+        {
+            var field = new Field(row, column);
+
+            Assert.Equal(1, field.Row);
+            Assert.Equal(2, field.Column);
+            Assert.Equal(FieldType.Empty, field.FieldType);
+        }
+    }
+}
